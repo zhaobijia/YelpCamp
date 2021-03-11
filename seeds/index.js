3f9bc38ada6f20f3a5f9be3ccdf3d1e22326f2b7
bijia <zhaobijia@gmail.com>
2021-03-11 16:30:28 -0500

    seeds campground databases

---
+++ seeds/index.js
@@ -0,0 +1,43 @@
+//connect to mongoose and contain the model
+//run this file on its own any time we want to seed our database.
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const Campground = require('../models/campground');
+const { places, descriptors } = require('./seedHelpers');
+
+//mongoose connect mongodb
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopolo: true,
+})
+//from https://mongoosejs.com/docs/
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    // we're connected!
+    console.log("Database connected");
+});
+
+//function for random selecting descriptors and placs
+const randSample = (array) => {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+const seedDB = async () => {
+    await Campground.deleteMany({})
+    for (let i = 0; i < 50; i++) {
+        const RandIndex = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            title: `${randSample(descriptors)} ${randSample(places)}`,
+            price: Math.floor(Math.random() * 20) + 10,
+            description: "description description description",
+            location: `${cities[RandIndex].city},  ${cities[RandIndex].state}`
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})

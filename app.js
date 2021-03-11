bc2f37b8a1056fbaef5be96710d8545286f2da9c
bijia <zhaobijia@gmail.com>
2021-03-11 17:06:09 -0500

    rendered all seeding campgrounds data

--- app.js
+++ app.js
@@ -27,7 +27,10 @@ app.get('/', (req, res) => {
     res.render('home');
 })
 
-
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds });
+})
 
 app.listen(3000, () => {
     console.log("LISTENING ON PORT 3000");

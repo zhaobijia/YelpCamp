f39d2563091b85ecc43b4dd65a266b93e1f8a77e
bijia <zhaobijia@gmail.com>
2021-03-11 17:29:44 -0500

    show route for each campground

--- app.js
+++ app.js
@@ -32,6 +32,12 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', { campgrounds });
 })
 
+app.get('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/show', { campground })
+})
+
 app.listen(3000, () => {
     console.log("LISTENING ON PORT 3000");
 })

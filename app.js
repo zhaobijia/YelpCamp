5af88d9caf1f2ccaa19268032a9def5a498dc107
bijia <zhaobijia@gmail.com>
2021-03-13 13:31:24 -0500

    installed new ejs tool ejs-mate and created boilerplate to test out functions

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 
 const Campground = require('./models/campground');
@@ -19,6 +20,9 @@ db.once('open', function () {
     // we're connected!
     console.log("Database connected");
 });
+
+
+app.engine('ejs', ejsMate);
 
 
 app.set('view engine', 'ejs');

3dd408c7bbbf8613e00522319594e6fc2ec4dbe2
bijia <zhaobijia@gmail.com>
2021-03-23 16:44:18 -0400

    add auto login right after a new user registered (using passport function)

--- routes/users.js
+++ routes/users.js
@@ -15,8 +15,15 @@ router.post('/register', catchAsync(async (req, res) => {
         const { email, username, password } = req.body;
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password)
-        req.flash('success', 'Welcome to Yelp Camp!');
-        res.redirect('/campgrounds');
+
+        //auto login after register, this function needs an error callback
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+            req.flash('success', `Welcome! ${registeredUser.username}`);
+            res.redirect('/campgrounds');
+        });
+
+
     } catch (e) {
         req.flash('error', e.message);
         res.redirect('/register');

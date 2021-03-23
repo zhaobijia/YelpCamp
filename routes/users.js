7431cbec6be3b7212c942d3dc9ef82a5e87e15ec
bijia <zhaobijia@gmail.com>
2021-03-22 23:18:53 -0400

    register route functionality with passport

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError');
 const User = require('../models/user');
+const passport = require('passport');
 
 //do i need a joi schema to check user?
 //register
@@ -10,7 +11,17 @@ router.get('/register', (req, res) => {
     res.render('users/register');
 })
 router.post('/register', catchAsync(async (req, res) => {
-    res.send(req.body);
+    try {
+        const { email, username, password } = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password)
+        req.flash('success', 'Welcome to Yelp Camp!');
+        res.redirect('/campgrounds');
+    } catch (e) {
+        req.flash('error', e.message);
+        res.redirect('/register');
+    }
+
 }))
 //login
 router.get('/login', (req, res) => {

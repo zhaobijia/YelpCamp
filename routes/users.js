63fe7a24ed6fd00c315dc7bcea3bdf220a2e0c44
bijia <zhaobijia@gmail.com>
2021-03-23 14:41:11 -0400

    add logout functionality using passport

--- routes/users.js
+++ routes/users.js
@@ -35,5 +35,10 @@ router.post('/login', passport.authenticate('local', { failureFlash: true, failu
 }))
 
 //logout
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success', "Goodbye!");
+    res.redirect('/campgrounds');
+})
 
 module.exports = router;

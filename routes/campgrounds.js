78e5ab2985de25fcaa165b20219b07cd4630735b
bijia <zhaobijia@gmail.com>
2021-03-29 15:41:04 -0400

    add multer, cloudianry, multer-storage-cloudianry to parse and upload images to cloudinary

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,11 +3,18 @@ const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const campgroundsController = require('../controllers/campgrounds');
 const { isLoggedIn, isCampAuthor, validateCampground } = require('../middleware');
+const multer = require('multer')
+const { storage } = require('../cloudinary');//node automatically look for index
+const upload = multer({ storage });
 
 
 router.route('/')
     .get(catchAsync(campgroundsController.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgroundsController.createCamp));
+    // .post(isLoggedIn, validateCampground, catchAsync(campgroundsController.createCamp));
+    .post(upload.array('campground[image]'), (req, res) => {
+        console.log(req.body, req.files);
+        res.send("worked?");
+    })
 
 //create campground form
 router.get('/new', isLoggedIn, catchAsync(campgroundsController.newForm));

f58583365cc27e8cacba4ffde1b2649b308ea25f
bijia <zhaobijia@gmail.com>
2021-03-30 23:21:53 -0400

    changed map style

--- public/javascripts/showMap.js
+++ public/javascripts/showMap.js
@@ -4,11 +4,17 @@ mapboxgl.accessToken = maptoken;
 const camp = JSON.parse(campground);
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    style: 'mapbox://styles/mapbox/outdoors-v11', // style URL
     center: camp.geometry.coordinates, // starting position [lng, lat]
     zoom: 7 // starting zoom
 });
 
 var marker = new mapboxgl.Marker()
     .setLngLat(camp.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 20 })
+            .setHTML(
+                `<h5>${camp.title}</h5>`
+            )
+    )
     .addTo(map);

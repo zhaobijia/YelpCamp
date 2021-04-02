a125826efdfca196968d8d51d8c1b69faa22ad7b
bijia <zhaobijia@gmail.com>
2021-04-02 16:52:43 -0400

    add sanitize html to joi extension

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const User = require('./models/user');
 
@@ -47,15 +48,18 @@ app.set("views", path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize());
 
 
 
 const sessionConfig = {
+    name: 'taotao',
     secret: 'thisshouldbeabettersecret!',
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        // secure:true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
